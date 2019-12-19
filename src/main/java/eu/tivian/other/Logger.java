21400bd55463db0329b573f7d48060d595547133
Paweł Kania <pawel.kania@onet.pl>
2019-12-19 11:47:11 +0100

    Ignore warning and error calls from debug functions

--- src/main/java/eu/tivian/other/Logger.java
+++ src/main/java/eu/tivian/other/Logger.java
@@ -1,13 +1,30 @@
 package eu.tivian.other;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Logger {
     public static final boolean ENABLE = true;
+    public static final Set<String> ignoreMethods = new HashSet<>();
+
     private static final java.util.logging.Logger log;
+    private static Exception ex = null;
 
     static {
         log = java.util.logging.Logger.getLogger(java.util.logging.Logger.GLOBAL_LOGGER_NAME);
         System.setProperty("java.util.logging.SimpleFormatter.format",
             "[%1$tT] [%4$s] %5$s %n");
+
+        ignoreMethods.add("log");
+    }
+
+    private static boolean ignore() {
+        for (var trace : Thread.currentThread().getStackTrace()) {
+            if (ignoreMethods.contains(trace.getMethodName()))
+                return true;
+        }
+
+        return false;
     }
 
     public static void info(String msg) {
@@ -15,10 +32,12 @@ public class Logger {
     }
 
     public static void warn(String msg) {
-        log.warning(msg);
+        if (!ignore())
+            log.warning(msg);
     }
 
     public static void error(String msg) {
-        log.severe(msg);
+        if (!ignore())
+            log.severe(msg);
     }
 }
